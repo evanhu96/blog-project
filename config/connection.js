65e74334c2d28e58fb9e7544f189be08bc93c246
evanhu96 <evanhughes66@yahoo.com>
2023-01-22 14:49:22 -0700

    dfs

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    DB_NAME='crowdfund_db',
-    DB_USER='root',
-    DB_PASSWORD='Ekh12518!',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
